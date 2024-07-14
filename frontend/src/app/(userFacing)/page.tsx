31c39a44e5bce5f051190d36cbf37bf6665cce2d
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2024-07-15 05:24:00 +0530

    feat: main page updated with mainCard

--- frontend/src/app/(userFacing)/page.tsx
+++ frontend/src/app/(userFacing)/page.tsx
@@ -1,20 +1,23 @@
-import TransactionCard from "@/components/TransactionCard";
 import MainCard from "@/components/mainCard";
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import TransactionCard from "@/components/TransactionCard";
+import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-
 
 export default function HomePage() {
 
     return (
         <>
-            <MainCard />
+
+        <MainCard />
+
+
 
             <Separator className="m-5" />
 
             <div className="flex flex-col">
 
                 <h1 className="text-3xl  font-semibold text-center m-2 ">Latest Transactions</h1>
+                <Button size={"sm"} className="w-fit m-2">Add New Transaction</Button>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
 
                     <TransactionCard />
@@ -24,8 +27,13 @@ export default function HomePage() {
 
                 </div>
 
+
+
             </div>
+
+
 
         </>
     )
 }
+
--- frontend/src/components/mainCard.tsx
+++ frontend/src/components/mainCard.tsx
@@ -1,37 +1,37 @@
+"use client"
 import { formatCurrency } from "@/lib/formatter";
 import { Card, CardHeader, CardTitle, CardDescription } from "./ui/card";
-import { Separator } from "./ui/separator";
+import { useState } from "react";
 
-export default function MainCard(){
+export default function MainCard() {
 
-    return <div className="flex justify-center align-middle">
-    <Card className="w-full sm:w-3/4 md:1/2 rounded-2xl h-fit bg-[#8eb0cd4a] shadow-lg" >
-        <CardHeader>
-            <CardTitle>
-                <div className="text-center">
-                    <h1 className="text-3xl">Total Balance</h1>
-                    <p className="text-2xl">{formatCurrency(0.00)}</p>
-                </div>
-            </CardTitle>
-            <CardDescription>
-                <div className="flex flex-row justify-between">
+    //usestate add
+    const[totalBalance, setTotalBalance] = useState<number>(0.00);
+    const[income, setIncome] = useState<number>(0.00);
+    const[expenses, setExpenses] = useState<number>(0.00);
 
-                    <div>
-                        <h2 className="text-lg">Income</h2>
-                        <p className="text-2xl">{formatCurrency(0.00)}</p>
+    return <div className="flex justify-center items-center">
+        <Card className="w-full sm:w-3/4 md:1/2 rounded-2xl h-fit bg-[#8eb0cd4a] shadow-lg">
+            <CardHeader>
+                <CardTitle>
+                    <div className="text-center">
+                        <h1 className="text-3xl">Total Balance</h1>
+                        <p className="text-2xl">{formatCurrency(totalBalance)}</p>
                     </div>
-
-                    <div>
-                        <h2 className="text-lg">Expenses</h2>
-                        <p className="text-2xl">{formatCurrency(0.00)}</p>
+                </CardTitle>
+                <CardDescription>
+                    <div className="flex flex-row justify-between">
+                        <div>
+                            <h2 className="text-lg">Income</h2>
+                            <p className="text-2xl">{formatCurrency(income)}</p>
+                        </div>
+                        <div>
+                            <h2 className="text-lg">Expenses</h2>
+                            <p className="text-2xl">{formatCurrency(expenses)}</p>
+                        </div>
                     </div>
-
-                </div>
-                
-            </CardDescription>
-        </CardHeader>
-
-    </Card>
-
-</div>
+                </CardDescription>
+            </CardHeader>
+        </Card>
+    </div>
 }
