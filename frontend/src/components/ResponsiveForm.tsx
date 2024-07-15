7c4fddc86934096bdb14930f24960bfb4f8958ab
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2024-07-15 12:13:57 +0530

    feat: dynamic form component created
    -responsive
    -drawer and dialog box changes dynamically

---
+++ frontend/src/components/ResponsiveForm.tsx
@@ -0,0 +1,113 @@
+import { formatCurrency } from "@/lib/formatter"
+import { Dialog, DialogTrigger, DialogContent, DialogTitle, DialogDescription } from "./ui/dialog"
+import TransactionCard from "./TransactionCard"
+import { DialogHeader, DialogFooter } from "./ui/dialog"
+import { Drawer,DrawerTrigger, DrawerContent, DrawerHeader, DrawerTitle, DrawerDescription, DrawerFooter, DrawerClose } from "./ui/drawer"
+import React, { ReactNode, useEffect, useState } from "react"
+import { Input } from "./ui/input"
+import { Button } from "./ui/button"
+
+type ResponsiveFormProps = {
+    children: ReactNode;
+    handleSubmit: () => void;
+}
+
+export default function ResponsiveForm({ children, handleSubmit }: ResponsiveFormProps) {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+    const [date, setDate] = useState<string>();
+    const [amount, setAmount] = useState<number>();
+
+    const handleResize = () => {
+        setIsMobile(window.innerWidth < 768);
+    }
+
+    
+
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, [])
+
+    return (
+        <>
+
+            {
+                isMobile ? (
+                    <Drawer>
+                        <DrawerTrigger className="w-full">
+                            {children}
+                        </DrawerTrigger>
+                        <DrawerContent>
+                            <DrawerHeader>
+                                <DrawerTitle>Update Transaction</DrawerTitle>
+                                <DrawerDescription>Enter Amount in LKR</DrawerDescription>
+                            </DrawerHeader>
+                            <div className="m-5">
+
+                                <div className="w-full">
+                                    <Input type="number" placeholder="Amount" value={amount ?? 0} onChange={(e) => { setAmount(parseInt(e.target.value)) }} />
+                                    {<span className="m-2 text-sm text-slate-300">{formatCurrency(amount ?? 0)}</span>}
+                                    <Input type="date" placeholder="Date and time" value={date} />
+                                </div>
+                            </div>
+                            <DrawerFooter>
+                                <div className="grid grid-cols-1 m-3 gap-1">
+                                    <Button onClick={handleSubmit}>Update</Button>
+                                    <DrawerClose asChild>
+                                        <Button variant={"outline"}>Cancel</Button>
+                                    </DrawerClose>
+
+                                </div>
+                            </DrawerFooter>
+                        </DrawerContent>
+
+                    </Drawer>
+
+                ) : (
+
+                    <Dialog>
+                        <DialogTrigger className="w-full">
+                            {children}
+                        </DialogTrigger>
+
+                        <DialogContent>
+                            <DialogHeader>
+                                <DialogTitle>Update Transaction</DialogTitle>
+                                <DialogDescription>Enter Amount in LKR</DialogDescription>
+                            </DialogHeader>
+                            <div className="m-5">
+
+                                <div className="w-full grid gap-4">
+                                    <Input type="number" placeholder="Amount" value={amount} onChange={(e) => { setAmount(parseInt(e.target.value)) }} />
+                                    <Input type="date" placeholder="Date and time" value={date} />
+                                </div>
+                            </div>
+
+                            <DialogFooter>
+                                <div className="flex m-3 gap-1">
+                                    <DrawerClose asChild>
+                                        <Button variant={"outline"}>Cancel</Button>
+                                    </DrawerClose>
+                                    <DrawerClose asChild>
+                                        <Button onClick={()=>{handleSubmit}}>Update</Button>
+                                    </DrawerClose>
+
+
+                                </div>
+                            </DialogFooter>
+
+                        </DialogContent>
+
+                    </Dialog>
+
+
+                )
+            }
+        </>
+
+    )
+}
