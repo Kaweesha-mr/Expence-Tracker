e9261f43b94dc084bfdc9c0def7dccb76d88e8b8
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2024-07-15 12:40:20 +0530

    fix:wrapped with form componnet

--- frontend/src/app/(userFacing)/page.tsx
+++ frontend/src/app/(userFacing)/page.tsx
@@ -1,4 +1,6 @@
+"use client"
 import MainCard from "@/components/mainCard";
+import ResponsiveForm from "@/components/ResponsiveForm";
 import TransactionCard from "@/components/TransactionCard";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
@@ -8,19 +10,23 @@ export default function HomePage() {
     return (
         <>
 
-        <MainCard totalBalance={2} expenses={2} income={1} />
+            <MainCard totalBalance={2} expenses={2} income={1} />
             <Separator className="m-5" />
-            
+
             <div className="grid grid-cols-1">
                 <h1 className="text-3xl  font-semibold text-center m-2 ">Latest Transactions</h1>
-                <Button size={"sm"} className="w-full md:w-fit mb-2">Add New Transaction</Button>
+
+                <ResponsiveForm handleSubmit={() => { }}>
+                    <Button size={"sm"} className="w-full m-5 md:w-fit ">Add New Transaction</Button>
+                </ResponsiveForm>
+
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                    <TransactionCard Title={"baba"}  Date={"2023/12/32"} Amount={20}/>
-                    <TransactionCard Title={"baba"}  Date={"2023/12/32"} Amount={20}/>
-                    <TransactionCard Title={"baba"}  Date={"2023/12/32"} Amount={20}/>
-                    <TransactionCard Title={"baba"}  Date={"2023/12/32"} Amount={20}/>
+                    <TransactionCard Title={"baba"} Date={"2023/12/32"} Amount={20} />
+                    <TransactionCard Title={"baba"} Date={"2023/12/32"} Amount={20} />
+                    <TransactionCard Title={"baba"} Date={"2023/12/32"} Amount={20} />
+                    <TransactionCard Title={"baba"} Date={"2023/12/32"} Amount={20} />
                 </div>
-            
+
             </div>
         </>
     )
--- frontend/src/app/(userFacing)/transactions/page.tsx
+++ frontend/src/app/(userFacing)/transactions/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import ResponsiveForm from "@/components/ResponsiveForm";
 import TransactionCard from "@/components/TransactionCard";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogHeader, DialogDescription, DialogContent, DialogTitle, DialogTrigger, DialogFooter } from "@/components/ui/dialog";
@@ -9,106 +10,15 @@ import { useEffect, useState } from "react";
 
 export default function Transactions() {
 
-    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
-    const [date, setDate] = useState<string>();
-    const [amount, setAmount] = useState<number>();
-
-    const handleResize = () => {
-        setIsMobile(window.innerWidth < 768);
-    }
 
     const handleUpdate = () => {
         //backend connection
         console.log("Update Transaction")
     }
 
-    useEffect(() => {
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        }
-    }, [])
     return (
-        <>
-
-            {
-                isMobile ? (
-                    <Drawer>
-                        <DrawerTrigger className="w-full">
-                            <TransactionCard Title="Payment" Date="12/12/2021" Amount={1000} />
-                        </DrawerTrigger>
-                        <DrawerContent>
-                            <DrawerHeader>
-                                <DrawerTitle>Update Transaction</DrawerTitle>
-                                <DrawerDescription>Enter Amount in LKR</DrawerDescription>
-                            </DrawerHeader>
-                            <div className="m-5">
-
-                                <div className="w-full">
-                                    <Input type="number" placeholder="Amount" value={amount} onChange={(e) => { setAmount(parseInt(e.target.value)) }} />
-                                    {<span className="m-2 text-sm text-slate-300">{formatCurrency(amount)}</span>}
-                                    <Input type="date" placeholder="Date and time" value={date} />
-                                </div>
-                            </div>
-                            <DrawerFooter>
-                                <div className="grid grid-cols-1 m-3 gap-1">
-                                    <Button onClick={handleUpdate}>Update</Button>
-                                    <DrawerClose asChild>
-                                        <Button variant={"outline"}>Cancel</Button>
-                                    </DrawerClose>
-
-                                </div>
-                            </DrawerFooter>
-                        </DrawerContent>
-
-                    </Drawer>
-
-                ) : (
-
-                    <Dialog>
-                        <DialogTrigger className="w-full">
-                            <TransactionCard Title="Payment" Date="12/12/2021" Amount={1000} />
-                        </DialogTrigger>
-
-                        <DialogContent>
-                            <DialogHeader>
-                                <DialogTitle>Update Transaction</DialogTitle>
-                                <DialogDescription>Enter Amount in LKR</DialogDescription>
-                            </DialogHeader>
-                            <div className="m-5">
-
-                                <div className="w-full grid gap-4">
-                                    <Input type="number" placeholder="Amount" value={amount} onChange={(e) => { setAmount(parseInt(e.target.value)) }} />
-                                    <Input type="date" placeholder="Date and time" value={date} />
-                                </div>
-                            </div>
-
-                            <DialogFooter>
-                                <div className="flex m-3 gap-1">
-                                    <DrawerClose asChild>
-                                        <Button variant={"outline"}>Cancel</Button>
-                                    </DrawerClose>
-                                    <DrawerClose asChild>
-                                        <Button onClick={handleUpdate}>Update</Button>
-                                    </DrawerClose>
-
-
-                                </div>
-                            </DialogFooter>
-
-                        </DialogContent>
-
-                    </Dialog>
-
-
-                )
-            }
-
-
-
-
-
-        </>
+        <ResponsiveForm handleSubmit={handleUpdate}>
+            <TransactionCard Title="Payment" Date="12/12/2021" Amount={1000} />
+        </ResponsiveForm>
     )
 }
