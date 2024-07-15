a3956cfff733e5952a0d140a93c3809697300873
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2024-07-15 05:35:36 +0530

    feat: props added for main and transition card

--- frontend/src/components/TransactionCard.tsx
+++ frontend/src/components/TransactionCard.tsx
@@ -1,16 +1,22 @@
 import { formatCurrency } from "@/lib/formatter";
+import { title } from "process";
 
-export default function TransactionCard(){
+type TransactionCardProps = {
+    Title: string;
+    Date: string;
+    Amount: number;
 
+}
 
+export default function TransactionCard({Title, Date, Amount}: TransactionCardProps) {
     return (
         <div className=" h-35 flex flex-row justify-between p-4 bg-slate-50 rounded-lg shadow-md hover:shadow-sm transition-all duration-300 ">
             <div>
-                <h1 className="p text-xl">Baba aunty rice</h1>
-                <p className="text-slate-400">2020/12/2</p>
+                <h1 className="p text-xl">{Title}</h1>
+                <p className="text-slate-400">{Date}</p>
             </div>
             <div>
-                <h1 className="text-red-700">{formatCurrency(900)}</h1>
+                <h1 className="text-red-700">{formatCurrency(Amount)}</h1>
             </div>
         </div>
     )
--- frontend/src/components/mainCard.tsx
+++ frontend/src/components/mainCard.tsx
@@ -3,12 +3,15 @@ import { formatCurrency } from "@/lib/formatter";
 import { Card, CardHeader, CardTitle, CardDescription } from "./ui/card";
 import { useState } from "react";
 
-export default function MainCard() {
+type MainCardProps = {
+    totalBalance: number;
+    income: number;
+    expenses: number;
+    }
 
-    //usestate add
-    const[totalBalance, setTotalBalance] = useState<number>(0.00);
-    const[income, setIncome] = useState<number>(0.00);
-    const[expenses, setExpenses] = useState<number>(0.00);
+export default function MainCard({...props}: MainCardProps) {
+
+
 
     return <div className="flex justify-center items-center">
         <Card className="w-full sm:w-3/4 md:1/2 rounded-2xl h-fit bg-[#8eb0cd4a] shadow-lg">
@@ -16,18 +19,18 @@ export default function MainCard() {
                 <CardTitle>
                     <div className="text-center">
                         <h1 className="text-3xl">Total Balance</h1>
-                        <p className="text-2xl">{formatCurrency(totalBalance)}</p>
+                        <p className="text-2xl">{formatCurrency(props.totalBalance)}</p>
                     </div>
                 </CardTitle>
                 <CardDescription>
                     <div className="flex flex-row justify-between">
                         <div>
                             <h2 className="text-lg">Income</h2>
-                            <p className="text-2xl">{formatCurrency(income)}</p>
+                            <p className="text-2xl">{formatCurrency(props.income)}</p>
                         </div>
                         <div>
                             <h2 className="text-lg">Expenses</h2>
-                            <p className="text-2xl">{formatCurrency(expenses)}</p>
+                            <p className="text-2xl">{formatCurrency(props.expenses)}</p>
                         </div>
                     </div>
                 </CardDescription>
