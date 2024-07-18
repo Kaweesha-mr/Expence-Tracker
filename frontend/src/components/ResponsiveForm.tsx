8b21d45784efa24b6d278033ebf1e44459ab3a5f
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2024-07-18 20:29:47 +0530

    feat: select option added
    -select type of transaction

--- frontend/src/components/ResponsiveForm.tsx
+++ frontend/src/components/ResponsiveForm.tsx
@@ -2,42 +2,45 @@
 import { formatCurrency } from "@/lib/formatter"
 import { Dialog, DialogTrigger, DialogContent, DialogTitle, DialogDescription } from "./ui/dialog"
 import { DialogHeader, DialogFooter } from "./ui/dialog"
-import { Drawer,DrawerTrigger, DrawerContent, DrawerHeader, DrawerTitle, DrawerDescription, DrawerFooter, DrawerClose } from "./ui/drawer"
+import { Drawer, DrawerTrigger, DrawerContent, DrawerHeader, DrawerTitle, DrawerDescription, DrawerFooter, DrawerClose } from "./ui/drawer"
 import React, { ReactNode, useEffect, useState } from "react"
 import { Input } from "./ui/input"
 import { Button } from "./ui/button"
+import { Select,SelectTrigger,SelectContent,SelectItem,SelectValue } from "./ui/select"
+
 
 
 
 type ResponsiveFormProps = {
     children: ReactNode;
     handleSubmit: () => void;
-    title:string;
+    title: string;
 }
 
-export default function ResponsiveForm({ children, handleSubmit ,title}: ResponsiveFormProps) {
+export default function ResponsiveForm({ children, handleSubmit, title }: ResponsiveFormProps) {
 
     const [isMobile, setIsMobile] = useState<boolean>(false);
     const [date, setDate] = useState<string>();
     const [amount, setAmount] = useState<number>();
+    const [type, setType] = useState<string>();
 
 
     useEffect(() => {
-      // Function to update isMobile based on window width
-      function handleResize() {
-        setIsMobile(window.innerWidth < 768);
-      }
-    
-      // Initial update
-      handleResize();
-    
-      // Event listener for window resize
-      window.addEventListener('resize', handleResize);
-    
-      // Clean-up function
-      return () => {
-        window.removeEventListener('resize', handleResize);
-      };
+        // Function to update isMobile based on window width
+        function handleResize() {
+            setIsMobile(window.innerWidth < 768);
+        }
+
+        // Initial update
+        handleResize();
+
+        // Event listener for window resize
+        window.addEventListener('resize', handleResize);
+
+        // Clean-up function
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
     }, []);
 
     return (
@@ -59,12 +62,22 @@ export default function ResponsiveForm({ children, handleSubmit ,title}: Respons
                                 <div className="w-full">
                                     <Input type="number" placeholder="Amount" value={amount ?? 0} onChange={(e) => { setAmount(parseInt(e.target.value)) }} />
                                     {<span className="m-2 text-sm text-slate-300">{formatCurrency(amount ?? 0)}</span>}
-                                    <Input type="date" placeholder="Date and time" value={date} />
+                                    <Input type="date" className="mb-4" placeholder="Date and time" value={date} />
+
+                                    <Select onValueChange={(newValue) => setType(newValue)}>
+                                        <SelectTrigger className={`w-[180px] ${type === "income" ? "text-green-600" : type === "expense" ? "text-red-600" : "text-black"} `}>
+                                            <SelectValue placeholder="Type" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectItem className="text-green-600 " value="income">Income</SelectItem>
+                                            <SelectItem className="text-red-600" value="expense">Expense</SelectItem>
+                                        </SelectContent>
+                                    </Select>
                                 </div>
                             </div>
                             <DrawerFooter>
                                 <div className="grid grid-cols-1 m-3 gap-1">
-                                    <Button onClick={handleSubmit}>{title === 'New' ? "Add":"Update"}</Button>
+                                    <Button onClick={handleSubmit}>{title === 'New' ? "Add" : "Update"}</Button>
                                     <DrawerClose asChild>
                                         <Button variant={"outline"}>Cancel</Button>
                                     </DrawerClose>
@@ -92,6 +105,15 @@ export default function ResponsiveForm({ children, handleSubmit ,title}: Respons
                                 <div className="w-full grid gap-4">
                                     <Input type="number" placeholder="Amount" value={amount} onChange={(e) => { setAmount(parseInt(e.target.value)) }} />
                                     <Input type="date" placeholder="Date and time" value={date} />
+                                    <Select onValueChange={(newValue) => setType(newValue)}>
+                                        <SelectTrigger className={`w-[180px] ${type === "income" ? "text-green-600" : type === "expense" ? "text-red-600" : "text-black"} `}>
+                                            <SelectValue placeholder="Type" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectItem className="text-green-600 " value="income">Income</SelectItem>
+                                            <SelectItem className="text-red-600" value="expense">Expense</SelectItem>
+                                        </SelectContent>
+                                    </Select>
                                 </div>
                             </div>
 
@@ -101,7 +123,7 @@ export default function ResponsiveForm({ children, handleSubmit ,title}: Respons
                                         <Button variant={"outline"}>Cancel</Button>
                                     </DrawerClose>
                                     <DrawerClose asChild>
-                                        <Button onClick={()=>{handleSubmit}}>{title === 'New' ? "Add":"Update"}</Button>
+                                        <Button onClick={() => { handleSubmit }}>{title === 'New' ? "Add" : "Update"}</Button>
                                     </DrawerClose>
                                 </div>
                             </DialogFooter>
