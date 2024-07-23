354b2ad0209523a6d32445a9cdbcc8390cddfb0a
kaweesha-mr <kaweesha.mr@gmail.com>
2024-07-23 15:50:13 +0530

    feat: OAuth authentication added

--- backend/routes/transactionRoute.routes.js
+++ backend/routes/transactionRoute.routes.js
@@ -1,14 +1,15 @@
 const transactionController = require('../controllers/transactionController');
 const router = require('express').Router();
 const express = require('express');
+const { requiresAuth } = require('express-openid-connect');
 
 
-router.get('/:id',transactionController.getAllTransactions);
-router.get('/one/:id',transactionController.getTransactionsbyId);
-router.post('/',transactionController.addTransaction);
-router.put('/:id',transactionController.updateTransaction);
-router.get('/sum/:id',transactionController.getSumOfIncomeTransactionsByUserId);
-router.get('/sum/:id',transactionController.getSumOfExpenseTransactionsByUserId);
-router.delete('/:id',transactionController.deleteTransactions);
+router.get('/:id',requiresAuth(),transactionController.getAllTransactions);
+router.get('/one/:id',requiresAuth(),transactionController.getTransactionsbyId);
+router.post('/',requiresAuth(),transactionController.addTransaction);
+router.put('/:id',requiresAuth(),transactionController.updateTransaction);
+router.get('/sum/:id',requiresAuth(),transactionController.getSumOfIncomeTransactionsByUserId);
+router.get('/sum/:id',requiresAuth(),transactionController.getSumOfExpenseTransactionsByUserId);
+router.delete('/:id',requiresAuth(),transactionController.deleteTransactions);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,38 @@ const expenseRoutes = require('./routes/transactionRoute.routes');
 const app = express();
 const loggerMiddleware = require('./middleware/loggerMiddleware');
 const ConnectDb = require('./config/db');
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
+
 require('dotenv').config();
 app.use(bodyParser.json());
-
-
 app.use(loggerMiddleware);
 
-//default route to check if the server is running
-app.get('/',(req,res)=>{
-    res.send('Server is running');
-})
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3002',
+  clientID: 'oglfokRvKhAfFYWw4o3fNGwZE9VoKN5h',
+  issuerBaseURL: 'https://dev-z20h2zat20mqcozf.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+
+
 
 app.use('/api/transactions', expenseRoutes);
 
